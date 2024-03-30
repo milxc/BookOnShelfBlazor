203049fd1a2e1badf76b986a8cdc71159213677b
Milana Deriga <0032532@rijnijssel.nl>
2024-03-30 23:07:42 +0100

    Touched up some design (made navbar with some icons)

--- BookOnShelfBlazor/Program.cs
+++ BookOnShelfBlazor/Program.cs
@@ -28,7 +28,7 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString).EnableSensitiveDataLogging(),ServiceLifetime.Scoped);
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
-builder.Services.AddIdentityCore<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
+builder.Services.AddIdentityCore<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = false)
     .AddRoles<IdentityRole>()
     .AddEntityFrameworkStores<ApplicationDbContext>()
     .AddSignInManager()
